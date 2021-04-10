74dbf43e32973908d9d830764ae6ba2f9e38fe7a
Mark Milligan <mtm20176@gmail.com>
2021-04-10 19:50:29 +0000

    add install docker-compose steps and chmod

---
+++ install-docker-compose.sh
@@ -0,0 +1,5 @@
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# apply permissions
+sudo chmod +x /usr/local/bin/docker-compose
