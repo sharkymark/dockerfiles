bfd965f22ca3413ced50775b9a5d1a32b85f9881
Mark Milligan <mtm20176@gmail.com>
2023-08-27 16:00:39 +0000

    chore: vnc dockerfile and intellij-idea vnc dockerfile

--- vnc/vnc/setup/no_vnc.sh
+++ vnc/vnc/setup/no_vnc.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p "$NO_VNC_HOME/utils/websockify"
-wget -qO- https://github.com/novnc/noVNC/archive/v1.2.0.tar.gz | tar xz --strip 1 -C "$NO_VNC_HOME"
-wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C "$NO_VNC_HOME/utils/websockify"
-chmod +x -v "$NO_VNC_HOME"/utils/*.sh
+wget -qO- https://github.com/novnc/noVNC/archive/v1.4.0.tar.gz | tar xz --strip 1 -C "$NO_VNC_HOME"
+wget -qO- https://github.com/novnc/websockify/archive/v0.11.0.tar.gz | tar xz --strip 1 -C "$NO_VNC_HOME/utils/websockify"
 
-## Create index.html to forward automatically to `vnc.html`
-ln -s "$NO_VNC_HOME/vnc.html" "$NO_VNC_HOME/index.html"
+# 2023-08-27 comment - no launch.sh in noVNC 1.3.0 and higher
+# chmod +x -v "$NO_VNC_HOME"/utils/*.sh
+
+## Substitute an index with settings optimized for Coder e.g., auto-connect
+ln -s "$VNC_ROOT_DIR/index.html" "$NO_VNC_HOME/index.html"
